e7d1ee70aa1d92325cbdf6d0611592fd657e4cb7
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-03-03 09:55:20 +0900

    add GET /

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,13 @@ import fs from "fs";
 import { logfile } from "./envs";
 
 export const app = express();
+
+app.get('/', (req, res) => {
+    const stream = fs.createReadStream('../resource/index.html');
+    res.status(200).contentType('text/html');
+    stream.pipe(res);
+})
+
 app.get('/all.csv', (req, res) => {
     const stream = fs.createReadStream(logfile);
     res.status(200).contentType('text/csv');
