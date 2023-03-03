317c85456b85b5331ae44b16b5b401969b5d5b9d
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-03-03 09:58:37 +0900

    use express static

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,10 @@ import { logfile } from "./envs";
 
 export const app = express();
 
-app.get('/', (req, res) => {
-    const stream = fs.createReadStream('../resource/index.html');
-    res.status(200).contentType('text/html');
-    stream.pipe(res);
-})
-
 app.get('/all.csv', (req, res) => {
     const stream = fs.createReadStream(logfile);
     res.status(200).contentType('text/csv');
     stream.pipe(res);
 });
+
+app.use(express.static('../resource/'));
