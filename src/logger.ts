812881cbe315aad20e39dc8b6663d19d59b7d16d
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-03-09 08:20:24 +0900

    add eventemitter

--- src/logger.ts
+++ src/logger.ts
@@ -2,8 +2,14 @@ import fs from "fs";
 import { Transform } from "stream";
 import { ReadlineParser, SerialPort } from "serialport";
 import { logfile, sensorPath } from "./envs";
+import { TypedEventEmitter } from "./typedEventEmitter";
 
 fs.existsSync(logfile) || fs.writeFileSync(logfile, 'Time,Temperature,Humidity,CO2\n');
+
+export const eventEmitter = new TypedEventEmitter<{
+    data: [temp: number, hum: number, co2: number];
+}>();
+
 export default () => {
     const addTimeTransform = new Transform({
         transform(chunk, encoding, callback) {
@@ -26,4 +32,13 @@ export default () => {
     parser.pipe(addTimeTransform);
     addTimeTransform.pipe(process.stdout);
     addTimeTransform.pipe(fileWritestream);
+
+    parser.on('data', (chunk) => {
+        const line = ('' + chunk).trim();
+        const [temp, hum, co2] = line.split(',').map(v => +v);
+        if (!(temp && hum && co2)) {
+            return;
+        }
+        eventEmitter.emit('data', temp, hum, co2);
+    });
 };
