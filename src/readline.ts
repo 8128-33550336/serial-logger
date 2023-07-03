43dd258f19a0f6ce67fda3f0d5a1b522bc90a50c
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-07-03 18:21:33 +0900

    readline

---
+++ src/readline.ts
@@ -0,0 +1,28 @@
+import * as readline from 'node:readline';
+import { nowInfo } from "./nowInfo.js";
+
+export function replStart() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: '> ',
+    });
+
+    rl.prompt();
+
+    rl.on('line', (line) => {
+        const command = line.trim();
+        if (command === 'info') {
+            console.log(nowInfo());
+        } else if (command.startsWith('frc:')) {
+
+        } else {
+            console.log(`unknown '${line.trim()}'`);
+            break;
+        }
+        rl.prompt();
+    }).on('close', () => {
+        console.log('Have a great day!');
+        process.exit(0);
+    });
+}
