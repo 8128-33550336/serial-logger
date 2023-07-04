a44931dba56cb9f53a81ffd9e808efaf19192065
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-07-04 21:37:45 +0900

    add test

--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -1,5 +1,8 @@
 import request from "supertest";
 import { app } from '../src/server';
+import { Readable } from "node:stream";
+
+
 
 describe('server test', () => {
     test('index', (done) => {
@@ -8,5 +11,48 @@ describe('server test', () => {
             .expect(200)
             .expect('Content-Type', /html/)
             .end(done);
+    });
+    describe('csv test', () => {
+        const fileContent = `Time,Temperature,Humidity,CO2
+0,1,2,3
+4,5,6,7
+8,9,10,11`;
+        beforeAll(() => {
+            jest.mock('fs', () => {
+
+                const mock = jest.fn();
+
+                let readed = false;
+                const readable = new Readable({
+                    read() {
+                        if (!readed) {
+                            this.push(fileContent);
+                        }
+                        readed = true;
+                    }
+                });
+                mock.mockReturnValue(readable);
+                return {
+                    createReadStream: mock
+                };
+            });
+        });
+        test('get csv', (done) => {
+            /*const res = */request(app)
+                .get('/all.csv')
+                .expect(200)
+                .expect('Content-Type', /csv/)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    expect(res.text).toBe(fileContent);
+                    done();
+                });
+
+        });
+
+        afterAll(() => {
+            jest.unmock('fs');
+        });
+
     });
 });
