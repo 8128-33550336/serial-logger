aa38e3de618866777bc11492bbbe7ddb3eafc7fb
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-07-07 05:20:37 +0900

    fix

--- src/nowInfo.ts
+++ src/nowInfo.ts
@@ -1,3 +1,5 @@
+import { eventEmitter } from "./eventEmitter";
+
 let val: { temp: number, hum: number, co2: number; } = { temp: 0, hum: 0, co2: 0 };
 eventEmitter.on('data', (temp, hum, co2) => {
     val = { temp, hum, co2 };
