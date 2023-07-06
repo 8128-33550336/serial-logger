afc3b60156d2165d3e348793afd2a56d1f8bd7a9
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-07-07 05:39:44 +0900

    wrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { port } from "./envs";
 import logger from "./logger";
-import { app } from "./server";
+import { createServer } from "./server";
 
 const { frc } = logger();
-app.listen(port);
+createServer().listen(port);
--- src/server.ts
+++ src/server.ts
@@ -6,73 +6,75 @@ import { listenerType } from "./typedEventEmitter.js";
 import { eventEmitter } from "./eventEmitter.js";
 import { nowInfo } from "./nowInfo.js";
 
+export const createServer = () => {
+    const app = expressWs(express()).app;
 
-export const app = expressWs(express()).app;
-
-app.get('/all.csv', (req, res) => {
-    const stream = fs.createReadStream(logFile);
-    res.status(200).contentType('text/csv');
-    stream.pipe(res);
-});
-
-app.get('/all.txt', (req, res) => {
-    const stream = fs.createReadStream(logFile);
-    res.status(200).contentType('text/plain');
-    stream.pipe(res);
-});
-
-app.ws('/', (ws, req) => {
-    const listener: listenerType<typeof eventEmitter, 'data'> = (temp, hum, co2) => {
-        ws.send(JSON.stringify({ temp, hum, co2 }));
-    };
-
-    eventEmitter.on('data', listener);
-
-    let pongTimeout: NodeJS.Timeout | null = null;
-    const interval = setInterval(() => {
-        ws.send('ping');
-        pongTimeout = setTimeout(() => {
-            ws.close();
-        }, 3 * 1000);
-    }, 300 * 1000);
-
-    ws.on('close', () => {
-        if (pongTimeout !== null) {
-            clearTimeout(pongTimeout);
-        }
-        clearInterval(interval);
-        eventEmitter.off('data', listener);
+    app.get('/all.csv', (req, res) => {
+        const stream = fs.createReadStream(logFile);
+        res.status(200).contentType('text/csv');
+        stream.pipe(res);
     });
 
-    ws.on('message', (data) => {
-        const message = '' + data;
-        if (message === 'pong' && pongTimeout !== null) {
-            clearTimeout(pongTimeout);
-        }
+    app.get('/all.txt', (req, res) => {
+        const stream = fs.createReadStream(logFile);
+        res.status(200).contentType('text/plain');
+        stream.pipe(res);
     });
-});
 
-app.get('/info', (req, res) => {
-    const val = nowInfo();
-    res.json(val);
-});
+    app.ws('/', (ws, req) => {
+        const listener: listenerType<typeof eventEmitter, 'data'> = (temp, hum, co2) => {
+            ws.send(JSON.stringify({ temp, hum, co2 }));
+        };
 
-app.post('/frc', express.json(), (req, res) => {
-    const json = req.body as unknown;
-    if (typeof json !== 'object' || !json) {
-        return;
-    }
-    if (!('co2' in json) || typeof json.co2 !== 'number') {
-        return;
-    }
-    const co2 = json.co2;
-    if (Number.isNaN(co2)) {
-        return;
-    }
-    if (co2 < 400 || co2 > 1000) {
-        return;
-    }
-    
-});
+        eventEmitter.on('data', listener);
 
-app.use(express.static('./resource'));
+        let pongTimeout: NodeJS.Timeout | null = null;
+        const interval = setInterval(() => {
+            ws.send('ping');
+            pongTimeout = setTimeout(() => {
+                ws.close();
+            }, 3 * 1000);
+        }, 300 * 1000);
+
+        ws.on('close', () => {
+            if (pongTimeout !== null) {
+                clearTimeout(pongTimeout);
+            }
+            clearInterval(interval);
+            eventEmitter.off('data', listener);
+        });
+
+        ws.on('message', (data) => {
+            const message = '' + data;
+            if (message === 'pong' && pongTimeout !== null) {
+                clearTimeout(pongTimeout);
+            }
+        });
+    });
+
+    app.get('/info', (req, res) => {
+        const val = nowInfo();
+        res.json(val);
+    });
+
+    app.post('/frc', express.json(), (req, res) => {
+        const json = req.body as unknown;
+        if (typeof json !== 'object' || !json) {
+            return;
+        }
+        if (!('co2' in json) || typeof json.co2 !== 'number') {
+            return;
+        }
+        const co2 = json.co2;
+        if (Number.isNaN(co2)) {
+            return;
+        }
+        if (co2 < 400 || co2 > 1000) {
+            return;
+        }
+
+    });
+
+    app.use(express.static('./resource'));
+    return app;
+};
